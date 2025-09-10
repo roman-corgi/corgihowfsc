5dc589895b7b4ecc97cb928ddaa2117bb33977ca
Alexis Lau <alexis.yslau@gmail.com>
2025-09-10 13:28:46 -0700

    made gitlImage class compatible with both corgisim and compact model but we do not have the same output

--- corgihowfsc/utils/corgisim_gitl_frames.py
+++ corgihowfsc/utils/corgisim_gitl_frames.py
@@ -9,6 +9,7 @@ from howfsc.model.mode import CoronagraphMode
 import howfsc.util.check as check
 from howfsc.util.insertinto import insertinto as pad_crop
 from howfsc.util.loadyaml import loadyaml
+from howfsc.scripts.gitlframes import sim_gitlframe
 
 ## import packages
 from corgisim import scene
@@ -19,40 +20,29 @@ import proper
 from corgisim import outputs
 import time
 
-# Gitl Image class should be able to handle either corgisim, or calling cgisim, following the defintion from 
 class GitlImage:
-    def __init__(self, name, Vmag=2.56, sptype='A5V', ref_flag=False, is_noise_free=True, output_dim=51):
-        self._mode = 'excam' # default and the only mode we are using for the camera 
-        
+    # Gitl Image class should be able to handle either corgisim, or calling cgisim, following the defintion from sim_gitlframe in howfsc.util.gitlframes
+    def __init__(self, name, cfg=None, cstrat=None, Vmag=2.56, sptype='A5V', ref_flag=False, is_noise_free=True, output_dim=51):
+
+        # common configs
+        self._mode = 'excam'  # default and the only mode we are using for the camera
+
         if name not in ['corgihowfsc', 'cgi-howfsc']:
             raise ValueError("name must be 'corgihowfsc' or 'cgi-howfsc'")
-
         self.name = name
-        
+
         host_star_properties = {'Vmag': Vmag,
                                 'spectral_type': sptype,
                                 'magtype': 'vegamag',
                                 'ref_flag': ref_flag}
-
-        # Default CCD parameters for EXCAM, setup for only passing to cgisim
-        self.CCD_DEFAULT = {
-            'full_well_serial': 100000,
-            'full_well': 80000,
-            'dark_rate': 8e-4,
-            'cic_noise': 0.016,
-            'read_noise': 140.0,
-            'bias': 1000,
-            'cr_rate': 0,
-            'e_per_dn': 6,
-            'nbits': 14,
-            'numel_gain_register': 604,
-        }
+        # cgi-howfsc configs
+        self.cfg = cfg 
+        self.cstrat = cstrat
         
         # corgisim configs
         self.is_noise_free = is_noise_free # corgisim only 
         self.output_dim = output_dim
 
-        # CORGISIM configs
         point_source_info = [] # default is just none, tbc whether there should be point source or not
         self.base_scene = scene.Scene(host_star_properties, point_source_info)
 
@@ -79,9 +69,7 @@ class GitlImage:
           1024, the number of active area cols on the EXCAM detector; under nominal
           conditions, there should be no reason to use anything else.
         """
-        # TODO - identify the common config and setup for both implementations 
         # Check inputs
-        print('Checking inputs for gitlframe generation ...')
         for dm in [dm1v, dm2v]:
             check.twoD_array(dm, 'dm', TypeError)
             nact = 48
@@ -97,14 +85,12 @@ class GitlImage:
         if self.name not in ['corgihowfsc', 'cgi-howfsc']:
             raise ValueError("name must be 'corgihowfsc' or 'cgi-howfsc'")
         elif self.name == 'corgihowfsc':
-            # do corgisim check
-            # TODO - verify if we need test unique to corgisim
+            # do corgisim check - verify if we need test unique to corgisim
             pass 
 
         elif self.name == 'cgi-howfsc':
             print('Using cgi-howfsc input checks ...')
             # do cgi-howfsc check 
-            # cgi-howfsc version only checks peakflux, but corgisim version checks exptime
             check.real_positive_scalar(exptime, 'exptime', TypeError)
 
             if not isinstance(crop, tuple):
@@ -117,7 +103,6 @@ class GitlImage:
             check.positive_scalar_integer(crop[2], 'crop[2]', TypeError)
             check.positive_scalar_integer(crop[3], 'crop[3]', TypeError)
 
-            # FIXME - if it is a noise free case, we do not need to check the peakflux and exptime 
             check.real_positive_scalar(exptime, 'exptime', TypeError)
             check.real_positive_scalar(peakflux, 'peakflux', TypeError)
 
@@ -125,6 +110,7 @@ class GitlImage:
 
             if lind >= len(cfg.sl_list):
                 raise ValueError('lind must be < len(cfg.sl_list)')
+
             check.positive_scalar_integer(cleanrow, 'cleanrow', TypeError)
             check.positive_scalar_integer(cleancol, 'cleancol', TypeError)
 
@@ -133,13 +119,9 @@ class GitlImage:
             dm1v, dm2v,
             cor, bandpass,
             crop,
-            param_dict,
-            ccd,
+            emccd_dict,
             polaxis,
             cleanrow, cleancol, fixedbp, wfe=None):
-
-        # # Frozen inputs
-        # fixedbp = np.zeros((cleanrow, cleancol), dtype=bool)  # TEMPORARY - 
 
         self.check_gitlframeinputs(dm1v, dm2v, fixedbp, exptime=exptime, crop=crop, cleanrow=cleanrow, cleancol=cleancol)
 
@@ -154,79 +136,57 @@ class GitlImage:
         image_star_corgi = sim_scene.host_star_image.data
 
         # emccd parameters for excam
-        emccd_keywords = {'em_gain': gain, 'cr_rate': 0}
-        detector = instrument.CorgiDetector(emccd_keywords)
+        if emccd_dict is None: 
+            emccd_dict = {'em_gain': gain, 'cr_rate': 0}
+
+        detector = instrument.CorgiDetector(emccd_dict)
 
         if self.is_noise_free:
             frame = sim_scene.host_star_image.data 
         else: 
-            # TODO - check if we need to crop the image here, or later
             sim_scene = detector.generate_detector_image(sim_scene, exptime)
             image_tot_corgi_sub = sim_scene.image_on_detector.data
             frame = image_tot_corgi_sub
 
         return frame
 
-    def gitlframe_cgihowfsc(self, exptime, gain,
-            dm1v, dm2v,
-            cor, bandpass,
-            crop,
-            param_dict,
-            ccd,
-            polaxis,
-            cleanrow, cleancol, 
-            fixedbp,
-            wfe=None):
+    def gitlframe_cgihowfsc(self, cfg, dmlist, peakflux, fixedbp, exptime, crop, lind, cleanrow=1024, cleancol=1024, wfe=None):
 
-        # Frozen inputs
-        # TEMPORARY. For cgi-howfsc, it should be imported from cstrat (a class containing variables for control strategy)
-        self.check_gitlframeinputs(dm1v, dm2v, fixedbp, exptime=exptime, crop=crop, cleanrow=cleanrow, cleancol=cleancol)
+        frame = sim_gitlframe(cfg, dmlist, fixedbp, peakflux, exptime, crop, lind, cleanrow=1024, cleancol=1024)
 
-        # FIXME - maek this work
-        # sim_gitlframe(cfg, dmlist, fixedbp, peakflux, exptime, crop, lind,
-        #                 cleanrow=1024, cleancol=1024)
-        # TODO - call sim_gitlframe
-
-        pass
+        return frame
 
     def get_image(self, exptime, gain,
             dm1v, dm2v,
             cor, bandpass,
+            lind=0,
+            peakflux=None, 
             crop=None,
-            param_dict={},
-            ccd=None,
+            emccd_dict=None,
             polaxis=10,
             cleanrow=1024, cleancol=1024, 
             fixedbp=np.zeros((1024, 1024), dtype=bool),
             wfe=None):
         """
-        Placeholder method to get a frame given a wavefront error map
+        Get a simulated GITL frame using either corgisim or cgi-howfsc repo's optical model
 
-        wfe is a placeholder argument for now to keep the option of passing additional wavefront error (
-        e.g. zernike coefficients) to modify the frame generation
+        wfe is a placeholder argument for now to keep the option of passing additional wavefront error (e.g. zernike coefficients) to modify the frame generation
 
         This get_image method should be compatible with both cgi-howfsc and corgisim 
         """
         if self.name == 'corgihowfsc':
-            self.gitlframe_corgisim(exptime, gain,
+            frame = self.gitlframe_corgisim(exptime, gain,
                 dm1v, dm2v,
                 cor, bandpass,
                 crop=crop,
-                param_dict=param_dict,
-                ccd=ccd,
+                emccd_dict=emccd_dict,
                 polaxis=polaxis,
                 cleanrow=cleanrow, cleancol=cleancol,
                 fixedbp=fixedbp,
                 wfe=wfe)
 
         elif self.name == 'cgi-howfsc':
-            self.gitlframe_cgihowfsc(exptime, gain,
-                dm1v, dm2v,
-                cor, bandpass,
-                crop=crop,
-                param_dict=param_dict,
-                ccd=ccd,
-                polaxis=polaxis,
-                cleanrow=cleanrow, cleancol=cleancol,
-                fixedbp=fixedbp,
-                wfe=wfe)
+            dmlist = [dm1v, dm2v] 
+            frame = self.gitlframe_cgihowfsc(self.cfg, dmlist, peakflux, self.cstrat.fixedbp, exptime, crop, lind, cleanrow=1024, cleancol=1024, wfe=None)
+
+        return frame
