2ce4c5b0de885beff0a32675dd19b2e4606a0c39
Alexis Lau <alexis.yslau@gmail.com>
2025-10-29 15:19:42 +0100

    Adding the correct library

--- corgihowfsc/utils/contrast_nomalization.py
+++ corgihowfsc/utils/contrast_nomalization.py
@@ -1,4 +1,8 @@
 from eetc import cgi_eetc
+from corgihowfsc.utils.corgisim_utils import _extract_host_properties_from_hconf, calculate_mas_per_lamD
+import numpy as np
+from corgihowfsc.utils.corgisim_manager import CorgisimManager
+
 
 class Normalization():
     # def __init__(self):
@@ -27,8 +31,31 @@ class EETCNormalization(Normalization):
 
 
 class CorgiNormalization(Normalization):
-    def __init__(self):
+    def __init__(self, cfg, cstrat, hconf, cor=None, corgi_overrides=None, separation_lamD=None):
         super().__init__()
 
-    def calc_flux_rate(self, hconf):
-        raise NotImplementedError()
+        self.separation_lamD = separation_lamD
+
+        # Initialize corgisime manager
+        self.corgisim_manager = CorgisimManager(cfg, cstrat, hconf, cor, corgi_overrides=corgi_overrides)
+
+    def calc_flux_rate(self, dm1v, dm2v, lind, exptime, gain=1.):
+        # TODO - image on the detector - noise case 
+        
+        # Inject off-axis source at specified separation at central wavelength
+        mas_per_lamD = calculate_mas_per_lamD(self.corgisim_manager.cfg.sl_list[1].lam)
+        
+        dy = self.separation_lamD * mas_per_lamD
+        dx = 0.
+
+        image_comp_corgi = self.corgisim_manager.generate_off_axis_psf(dm1v, dm2v, dx, dy, lind=lind, exptime=exptime, gain=gain)
+    
+        return np.max(image_comp_corgi)
+
+    def normalize(self, im, peakflux, exptime):
+        # TODO - image on the detector - noise case 
+        if exptime is None or self.corgisim_manager.is_noise_free:
+            exptime = 1.  # unit
+            return im / peakflux / exptime
+        else:
+            raise NotImplementedError("Normalization with noise case is not yet implemented.")
