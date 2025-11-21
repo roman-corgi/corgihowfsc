1cddb506d0fd486cd78f73b6a049a3e70f9142a4
Lukas <lukas.delaye@obspm.fr>
2025-11-21 11:49:19 +0100

    comments

--- corgihowfsc/sensing/Estimator_choice.py
+++ corgihowfsc/sensing/Estimator_choice.py
@@ -16,7 +16,7 @@ class DefaultEstimator(Estimator): # Returns the estimation of the electric fiel
 # This new class allows you to focus on the EFC by directly using the electric field propagated by the optical model rather than by PWP.
 # For stability reasons, the PWP is still performed according to my tests, it is just not used.
 
-class PerfectEstimator(Estimator): # Returns the perfect electric field directly from model_efield parameter
+class PerfectEstimator(Estimator): # Returns the perfect electric field directly from model_efield parameter : this version work with corgisim
 
     def __init__(self, model_efield=None): # Initialize with the perfect electric field
         self.model_efield = model_efield
