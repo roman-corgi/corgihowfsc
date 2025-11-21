38addd55541889c07ed21d48e9eade2d717bc433
Lukas <lukas.delaye@obspm.fr>
2025-11-21 16:22:39 +0100

    Add **kwargs

--- corgihowfsc/sensing/Estimator.py
+++ corgihowfsc/sensing/Estimator.py
@@ -8,6 +8,6 @@ class Estimator():
     # def __init__():
 
     def estimate_efield(self, intensities, phases,
-                    min_good_probes=2, eestclip=np.inf, eestcondlim=0):
+                    min_good_probes=2, eestclip=np.inf, eestcondlim=0, **kwargs):
 
         NotImplementedError()
