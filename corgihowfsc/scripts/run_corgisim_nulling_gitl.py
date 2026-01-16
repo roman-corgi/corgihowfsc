a4dad2d8a9a3a9c6f47355f9510bf1ba4e793531
A J Eldorado Riggs <aj.riggs@gmail.com>
2026-01-16 09:34:49 -0800

    Make nulling scripts more similar

--- corgihowfsc/scripts/run_corgisim_nulling_gitl.py
+++ corgihowfsc/scripts/run_corgisim_nulling_gitl.py
@@ -1,26 +1,15 @@
-
-import time
 import os
-import argparse
-import cProfile
-import pstats
-import logging
 from datetime import datetime
-import numpy as np
-import astropy.io.fits as pyfits
+import matplotlib
+matplotlib.use('TkAgg')
 
-import corgihowfsc
 import eetc
 from howfsc.control.cs import ControlStrategy
 from howfsc.model.mode import CoronagraphMode
 from howfsc.util.loadyaml import loadyaml
 
-import roman_preflight_proper
-### Then, run the following command to copy the default prescription file
-roman_preflight_proper.copy_here()
-
 import corgihowfsc
-from corgihowfsc.utils.howfsc_initialization import get_args, load_files, load_dm_start_maps
+from corgihowfsc.utils.howfsc_initialization import get_args, load_files
 from corgihowfsc.sensing.DefaultEstimator import DefaultEstimator
 from corgihowfsc.sensing.PerfectEstimator import PerfectEstimator
 from corgihowfsc.sensing.DefaultProbes import DefaultProbes
@@ -28,11 +17,14 @@ from corgihowfsc.utils.contrast_nomalization import CorgiNormalization, EETCNorm
 from corgihowfsc.gitl.nulling_gitl import nulling_gitl
 from corgihowfsc.utils.corgisim_gitl_frames import GitlImage
 
+import roman_preflight_proper
+### Then, run the following command to copy the default prescription file
+roman_preflight_proper.copy_here()
+
 eetc_path = os.path.dirname(os.path.abspath(eetc.__file__))
 howfscpath = os.path.dirname(os.path.abspath(corgihowfsc.__file__))
 defjacpath = os.path.join(os.path.dirname(howfscpath), 'temp')  # User should set to somewhere outside the repo
 
-# Note: MUST DEFINE JACPATH FOR CORGI GITL FRAMES
 precomp = 'precomp_jacs_always' #'load_all' if defjacpath is not None else 'precomp_all_once'
 current_datetime = datetime.now()
 folder_name = 'gitl_simulation_' + current_datetime.strftime("%Y-%m-%d_%H%M%S")
@@ -40,14 +32,20 @@ fits_name = 'final_frames.fits'
 fileout_path = os.path.join(os.path.dirname(os.path.dirname(corgihowfsc.__file__)), 'data', folder_name, fits_name)
 dmstartmap_filenames = ['iter_080_dm1.fits', 'iter_080_dm2.fits']
 
+
 def main(): 
-    args = get_args(mode='nfov_band1',
-                    precomp=precomp,
-                    num_process=2,
-                    num_threads=1,
-                    fileout=fileout_path,
-                    jacpath=defjacpath,
-                    dm_start_shape=dm_start_shape)
+
+    args = get_args(
+        mode='nfov_band1',
+        dark_hole='360deg',
+        probe_shape='default',
+        precomp=precomp,
+        num_process=2,
+        num_threads=1,
+        fileout=fileout_path,
+        jacpath=defjacpath,
+        dmstartmap_filenames=dmstartmap_filenames,
+    )
 
     # User params
     niter = args.niter
