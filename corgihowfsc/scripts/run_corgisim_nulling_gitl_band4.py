719517ecba51e918c7e023fc7a176dc3d929361d
Susan F Redmond <susan.m.redmond@jpl.nasa.gov>
2026-01-21 13:20:26 -0800

    added dedicated run script for band 4 WFOV

---
+++ corgihowfsc/scripts/run_corgisim_nulling_gitl_band4.py
@@ -0,0 +1,104 @@
+import os
+from datetime import datetime
+import matplotlib
+matplotlib.use('TkAgg')
+
+import eetc
+from howfsc.control.cs import ControlStrategy
+from howfsc.model.mode import CoronagraphMode
+from howfsc.util.loadyaml import loadyaml
+
+import roman_preflight_proper
+### Then, run the following command to copy the default prescription file
+roman_preflight_proper.copy_here()
+
+import corgihowfsc
+from corgihowfsc.utils.howfsc_initialization import get_args, load_files
+from corgihowfsc.sensing.DefaultEstimator import DefaultEstimator
+from corgihowfsc.sensing.PerfectEstimator import PerfectEstimator
+from corgihowfsc.sensing.DefaultProbes import DefaultProbes
+from corgihowfsc.utils.contrast_nomalization import CorgiNormalization, EETCNormalization
+from corgihowfsc.gitl.nulling_gitl import nulling_gitl
+from corgihowfsc.utils.corgisim_gitl_frames import GitlImage
+
+eetc_path = os.path.dirname(os.path.abspath(eetc.__file__))
+howfscpath = os.path.dirname(os.path.abspath(corgihowfsc.__file__))
+defjacpath = os.path.join(os.path.dirname(howfscpath), 'temp')  # User should set to somewhere outside the repo
+
+precomp = 'precomp_jacs_always' #'load_all' if defjacpath is not None else 'precomp_all_once'
+current_datetime = datetime.now()
+folder_name = 'gitl_simulation_wfov_band4_' + current_datetime.strftime("%Y-%m-%d_%H%M%S")
+fits_name = 'final_frames.fits'
+fileout_path = os.path.join(os.path.dirname(os.path.dirname(corgihowfsc.__file__)), 'data', folder_name, fits_name)
+dmstartmap_filenames = ['iter_061_dm1.fits', 'iter_061_dm2.fits']
+
+
+def main(): 
+
+    args = get_args(
+        mode='wfov_band4',
+        dark_hole='360deg',
+        probe_shape='default',
+        precomp=precomp,
+        num_process=2,
+        num_threads=1,
+        fileout=fileout_path,
+        jacpath=defjacpath,
+        dmstartmap_filenames=dmstartmap_filenames,
+    )
+
+    # User params
+    mode = args.mode
+
+    modelpath, cfgfile, jacfile, cstratfile, probefiles, hconffile, n2clistfiles, dmstartmaps = load_files(args, howfscpath)
+
+    # cfg
+    cfg = CoronagraphMode(cfgfile)
+
+    # hconffile
+    hconf = loadyaml(hconffile, custom_exception=TypeError)
+
+    # If it is desired to overwrite the stellar mag/type, do so here
+    # hconf['star']['stellar_vmag'] = 5
+    # hconf['star']['stellar_type'] = 'G0V'
+
+    # Define control and estimator strategy
+    cstrat = ControlStrategy(cstratfile)
+    estimator = DefaultEstimator()
+
+    # Initialize default probes class
+    probes = DefaultProbes('default')
+
+    # Image cropping parameters:
+    crop_params = {}
+    crop_params['nrow'] = 153
+    crop_params['ncol'] = 153
+    crop_params['lrow'] = 0
+    crop_params['lcol'] = 0
+
+    # Define imager and normalization (counts->contrast) strategy
+    corgi_overrides = {}
+    corgi_overrides['output_dim'] = crop_params['nrow']
+    corgi_overrides['is_noise_free'] = False
+    imager = GitlImage(
+        cfg=cfg,         # Your CoronagraphMode object
+        cstrat=cstrat,   # Your ControlStrategy object
+        hconf=hconf,      # Your host config with stellar properties
+        backend='corgihowfsc',
+        cor=mode,
+        corgi_overrides=corgi_overrides
+    )
+
+    # normalization_strategy = CorgiNormalization(cfg,
+    #                                             cstrat,
+    #                                             hconf,
+    #                                             cor=args.mode,
+    #                                             corgi_overrides=corgi_overrides,
+    #                                             separation_lamD=7,
+    #                                             exptime_norm=0.01)
+    normalization_strategy = EETCNormalization()
+
+    nulling_gitl(cstrat, estimator, probes, normalization_strategy, imager, cfg, args, hconf, modelpath, jacfile, probefiles, n2clistfiles, crop_params, dmstartmaps)
+
+if __name__ == '__main__':    
+    main()
