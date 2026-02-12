d26815bb6902a3a7a9ee71349714e26e65d545e5
Alexis Lau <40294148+alexisyslau@users.noreply.github.com>
2026-02-12 14:28:23 -0800

    Updated the documentation for calculate_jacobian

--- corgihowfsc/gitl/modular_gitl.py
+++ corgihowfsc/gitl/modular_gitl.py
@@ -495,7 +495,8 @@ def _main_howfsc_computation(framelist, dm1_list, dm2_list, cfg, jac, jtwj_map,
 
         # Get e-field from the imager, which will be either the compact model or corigisim model
 
-        model_efield = imager.get_efield(dm1v=dmlistmeas[0], dm2v=dmlistmeas[1], lind=j, crop=croplist[j * ndm])
+        # If this is corgisim, we only need to call get_efield once per wavelength
+        # since the DM settings don't affect the model e-field (since corgisim doesn't include DMs).  If this is the compact model, we need to call get_efield for each DM setting, since the DM settings do affect the model e-field.
 
         # Old legacy code to keep just in case we need future reference
         # edm0 = cfg.sl_list[j].eprop(dmlistmeas)
@@ -503,7 +504,20 @@ def _main_howfsc_computation(framelist, dm1_list, dm2_list, cfg, jac, jtwj_map,
         # edh0 = cfg.sl_list[j].proptodh(ely)
         # model_efield = insertinto(edh0, efield.shape)
 
-        other[j]['model_efield'] = model_efield # for reqt 1133640
+        perfect_efield = imager.get_efield(dm1v=dmlistmeas[0], dm2v=dmlistmeas[1], lind=j, crop=croplist[j * ndm])
+
+        if imager.backend == 'corgihowfsc':
+            # TODO - add a warning here for those who wants to speed up the corgisim by changing number of filters in cgisim_bandpasses
+            # TODO - normalisation of the model e-field?
+            log.info('Using corgisim model, so model e-field is same for all DM settings at a given wavelength')
+            mid_index = len(perfect_efield) // 2  # Get the central bandpass for the e-field
+            model_efield = perfect_efield[mid_index]
+        elif imager.backend == 'cgi-howfsc':
+            model_efield = perfect_efield
+        else:
+            raise ValueError(f"Unrecognized imager backend: {imager.backend}")
+
+        other[j]['model_efield'] = model_efield  # for reqt 1133640
 
         log.info('Compute difference to use to predict next iteration')
         dest = insertinto(efield - model_efield, cfg.sl_list[j].dh.e.shape)
