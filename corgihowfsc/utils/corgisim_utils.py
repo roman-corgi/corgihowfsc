6eec4780266a76fb4c779666a88269c67585d639
Alexis Lau <40294148+alexisyslau@users.noreply.github.com>
2026-02-26 04:50:37 +0100

    updated the mapping and their respective bandpass number below

--- corgihowfsc/utils/corgisim_utils.py
+++ corgihowfsc/utils/corgisim_utils.py
@@ -1,15 +1,16 @@
 import numpy as np
 
 # Mapping configuration - easy to update for new modes
+# NOTE - Add new mappings here as support is added
+
 CGI_TO_CORGI_MAPPING = {
     'narrowfov': 'hlc',
     'nfov_flat': 'hlc', 
     'nfov_dm': 'hlc',
     'nfov_band1': 'hlc',
-    'wfov_band4': 'spc-wide'
-    # NOTE - Add new mappings here as support is added
-    # 'widefov': 'widefov',  # Future support
-    # 'spec': 'spec',    # Future spectroscopy mode
+    'spec_band2': 'spc-spec_band2', 
+    'spec_band3': 'spc-spec_band3',
+    'wfov_band4': 'spc-wide',
 }
 
 SUPPORTED_CGI_MODES = list(CGI_TO_CORGI_MAPPING.keys())
@@ -47,7 +48,10 @@ def map_wavelength_to_corgisim_bandpass(wavelength_m, tolerance=3e-9):
         CorgiSim bandpass label ('1', '2', '3', or '4')
     """
     corgisim_wavelengths = {
-        '1': 575e-9, '2': 660e-9, '3': 730e-9, '4': 825e-9}
+        '1': 575e-9, # 1B
+        '2': 638e-9, # 2B
+        '3': 727e-9, # 3C
+        '4': 825e-9} # 4B
     
     for bandpass, wl in corgisim_wavelengths.items():
         if abs(wavelength_m - wl) <= tolerance:
