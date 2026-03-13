23868b114abb3ffe832554705c2afd9c5fc3b5ec
Alexis Lau <alexis.yslau@gmail.com>
2026-03-13 01:20:59 +0100

    removed safe_cpu_count. it was hardcoded to a number before

--- corgihowfsc/gitl/nulling_gitl.py
+++ corgihowfsc/gitl/nulling_gitl.py
@@ -262,8 +262,8 @@ def nulling_gitl(cstrat, estimator, probes, normalization_strategy, imager, cfg,
     # do last, needs peak flux
     rng = np.random.default_rng(12345)
     
-    safe_cpu_count = args.num_imager_worker # TODO - hard coeding
-    print('Using num_imager_worker = ', safe_cpu_count)
+    num_imager_worker = args.num_imager_worker 
+    print('Using num_imager_worker = ', num_imager_worker)
 
     # normalisation strategy first then imager, since normalisation strategy is needed to calculate peak flux for framelist collection
 
@@ -280,7 +280,7 @@ def nulling_gitl(cstrat, estimator, probes, normalization_strategy, imager, cfg,
         ndm=ndm,
         cstrat=cstrat,
         fracbadpix=fracbadpix,
-        n_jobs=safe_cpu_count,
+        n_jobs=num_imager_worker,
     )
 
     # drop packets for testing if requested
