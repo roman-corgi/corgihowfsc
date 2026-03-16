39188e271a1c97cfd550e9a9c73fc80c719208e8
Alexis Lau <alexis.yslau@gmail.com>
2026-03-16 18:30:17 +0100

    merging on-axis norm into corgisim_manager

--- corgihowfsc/utils/corgisim_manager.py
+++ corgihowfsc/utils/corgisim_manager.py
@@ -143,6 +143,8 @@ class CorgisimManager:
             'use_field_stop': 1
         }
 
+        optics_keywords['NCPUS'] = self.corgi_overrides.get('NCPUS', 7) # default to 7 cores if not specified in optics_keywords
+
         optics = instrument.CorgiOptics(
             self._mode,
             bandpass_recipe,
