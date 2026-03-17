36c1304e97c24da4cd15d39a220f3905f671f3ef
Alexis Lau <alexis.yslau@gmail.com>
2026-03-17 01:53:49 +0100

    added a check for cpu allocation and add a warning when oversubscribed

--- corgihowfsc/scripts/run_corgisim_nulling_gitl.py
+++ corgihowfsc/scripts/run_corgisim_nulling_gitl.py
@@ -15,7 +15,7 @@ import roman_preflight_proper
 roman_preflight_proper.copy_here()
 
 import corgihowfsc
-from corgihowfsc.utils.howfsc_initialization import get_args, load_files
+from corgihowfsc.utils.howfsc_initialization import get_args, load_files, get_cpu_allocation
 from corgihowfsc.sensing.DefaultEstimator import DefaultEstimator
 from corgihowfsc.sensing.PerfectEstimator import PerfectEstimator
 from corgihowfsc.sensing.GettingProbes import ProbesShapes
@@ -43,16 +43,13 @@ dmstartmap_filenames = ['iter_080_dm1.fits',
 output_every_iter = True  # Set to True to save frames at every iteration in real time, False to save all data after the simulation is complete. The file structure will be the same in both cases.
 
 # CPU count setup for parallel processing
-# CHECK - num_proper_process might need to be set to 1 when parallising corgisim?
 num_proper_process = 5 # Default is set by corgi_overrides in GitlImage initialization to 2. 
 num_jac_process = 12 # Default to 2 processes for Jacobian calculation, can be increased if needed. 
-
-# TODO - dummy numbers now but should be set and implemented later
-num_efield_worker = None
-num_imager_worker = 5
-num_corgisim_norm_worker = 1
+num_imager_worker = None
 
 def main():
+    global num_jac_process, num_imager_worker, num_proper_process
+
     # Desired mask, band, dark hole, and probe shape
     mode = 'nfov_band1'
     dark_hole = '360deg'
@@ -67,6 +64,9 @@ def main():
     fileout_path = make_output_file_structure(loop_framework, backend_type, base_path, base_corgiloop_path,
                                               final_filename, tag=folder_tag)
 
+    # Check the CPU allocation and give user a warning if the requested parallelization may exceed available resources
+    num_jac_process, num_imager_worker, num_proper_process = get_cpu_allocation(num_jac_process, num_imager_worker, num_proper_process)
+    
     args = get_args(
         niter=3,
         mode=mode,
@@ -81,9 +81,11 @@ def main():
         logfile=os.path.join(os.path.dirname(fileout_path), 'gitl.log')
     )
 
+
     # User params
     mode = args.mode
     args.num_imager_worker = num_imager_worker
+    args.num_proper_process = num_proper_process
 
     modelpath, cfgfile, jacfile, cstratfile, probefiles, hconffile, n2clistfiles, dmstartmaps = load_files(args,
                                                                                                            howfscpath)
