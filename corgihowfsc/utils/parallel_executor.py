7be27a1642bef1eb01b03ab499a31ef77b802370
Alexis Lau <alexis.yslau@gmail.com>
2026-04-01 19:14:12 +0200

    removed joblib multiproceessing code

--- corgihowfsc/utils/parallel_executor.py
+++ corgihowfsc/utils/parallel_executor.py
@@ -97,28 +97,3 @@ def run_parallel(
     ctx = mp.get_context(start_method)
     with ctx.Pool(processes=n_jobs) as pool:
         return pool.starmap(func, args_list)
-
-# Alternative implementation using joblib, but proper is using multiprocessing.Pool for parallel processing in this case -> not straightforward as it would crash ... due to nested parallelism. 
-
-# from joblib import Parallel, delayed
-
-# def run_parallel(fn, args_list, n_jobs=1):
-#     """
-#     Run fn over args_list in parallel using joblib.
-#     Blocks until ALL jobs finish (barrier).
-    
-#     Args:
-#         fn: callable — must be picklable (free function or static method)
-#         args_list: list of tuples — each tuple is unpacked as fn(*args)
-#         n_jobs: number of workers. 1 = serial, -1 = all cores (avoid when on cluster)
-    
-#     Returns:
-#         list of results in same order as args_list
-#     """
-#     if n_jobs == 1:
-#         return [fn(*a) for a in args_list]
-    
-#     return Parallel(n_jobs=n_jobs, backend='loky')(
-#         delayed(fn)(*a) for a in args_list
-#     )
-
