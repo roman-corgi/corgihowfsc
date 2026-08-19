6ebd4d009c8ba826b7c2524d8f0fc45f938d5145
Susan Redmond <43475978+sfr348@users.noreply.github.com>
2026-08-18 17:52:43 -0700

    Update model_registry.py

--- corgihowfsc/model/model_registry.py
+++ corgihowfsc/model/model_registry.py
@@ -36,17 +36,17 @@ ProbeFiles = namedtuple(
 
 PROBE_FILES = {
     'nfov_band1': ProbeFiles(
-        default=['nfov_dmrel_4_1.0e-05_cos.fits', 'nfov_dmrel_4_1.0e-05_sinlr.fits', 'nfov_dmrel_4_1.0e-05_sinud.fits'],
-        single=['nfov_dmrel_1.0e-05_act0.fits', 'nfov_dmrel_1.0e-05_act1.fits', 'nfov_dmrel_1.0e-05_act2.fits'],
-        gaussian=['nfov_dmrel_4_1.0e-05_gaussian0.fits', 'nfov_dmrel_4_1.0e-05_gaussian1.fits', 'nfov_dmrel_4_1.0e-05_gaussian2.fits'],
-        unmodulated_sinc=['nfov_dmrel_4_1.0e-05_sinc.fits', 'nfov_dmrel_4_1.0e-05_sinc_shifted_right.fits', 'nfov_dmrel_4_1.0e-05_sinc_shifted_diag_ur.fits'],
+        default=['nfov_band1_dmrel_4_1.0e-05_cos.fits', 'nfov_band1_dmrel_4_1.0e-05_sinlr.fits', 'nfov_band1_dmrel_4_1.0e-05_sinud.fits'],
+        single=['nfov_band1_dmrel_1.0e-05_act0.fits', 'nfov_band1_dmrel_1.0e-05_act1.fits', 'nfov_band1_dmrel_1.0e-05_act2.fits'],
+        gaussian=['nfov_band1_dmrel_4_1.0e-05_gaussian0.fits', 'nfov_band1_dmrel_4_1.0e-05_gaussian1.fits', 'nfov_band1_dmrel_4_1.0e-05_gaussian2.fits'],
+        unmodulated_sinc=['nfov_band1_dmrel_4_1.0e-05_sinc.fits', 'nfov_band1_dmrel_4_1.0e-05_sinc_shifted_right.fits', 'nfov_band1_dmrel_4_1.0e-05_sinc_shifted_diag_ur.fits'],
     ),
     'spec_band2': ProbeFiles(),
     'spec_band3': ProbeFiles(
-        default=['spec_dmrel_band3_ni1e-05_sin150_rot0.fits', 'spec_dmrel_band3_ni1e-05_sin210_rot0.fits', 'spec_dmrel_band3_ni1e-05_sin90_rot0.fits'],
+        default=['spec_band3_dmrel_ni1e-05_sin150_rot0.fits', 'spec_band3_dmrel_ni1e-05_sin210_rot0.fits', 'spec_band3_dmrel_ni1e-05_sin90_rot0.fits'],
     ),
     'wfov_band4': ProbeFiles(
-        default=['wfov_dmrel_1e-5_cos_constrained.fits', 'wfov_dmrel_1e-5_sinlr_constrained.fits', 'wfov_dmrel_1e-5_sinud_constrained.fits'],
+        default=['wfov_band4_dmrel_1e-5_cos_constrained.fits', 'wfov_band4_dmrel_1e-5_sinlr_constrained.fits', 'wfov_band4_dmrel_1e-5_sinud_constrained.fits'],
     ),
     'wfov_band1': ProbeFiles(
         default=['wfov_band1_dmrel_1e-5_cos.fits', 'wfov_band1_dmrel_1e-5_sinlr_constrained.fits', 'wfov_band1_dmrel_1e-5_sinud.fits']
