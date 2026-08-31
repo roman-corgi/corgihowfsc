5fb84a555d6bb74a75fd2bb8ae39ca7640ceb393
Alexis Lau <40294148+alexisyslau@users.noreply.github.com>
2026-08-31 15:16:16 -0400

    reset the default value to 0 for both bias and cr_rate

--- corgihowfsc/utils/corgisim_manager.py
+++ corgihowfsc/utils/corgisim_manager.py
@@ -123,8 +123,8 @@ class CorgisimManager:
         Initialize EMCCD parameters from overrides or defaults.
         Other parameeter can be added here as needed, otherwise they will take the default values from CorgiDetector.
         """
-        self.bias = self.emccd_overrides.get('bias', 1500) # default to 1500 
-        self.cr_rate = self.emccd_overrides.get('cr_rate', 5) # default to 5
+        self.bias = self.emccd_overrides.get('bias', 0) # default should be 1500
+        self.cr_rate = self.emccd_overrides.get('cr_rate', 0) # default should be 5
 
     def _initialize_base_scene(self):
         # Initialise scene object 
