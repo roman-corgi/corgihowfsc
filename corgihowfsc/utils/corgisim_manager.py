a5a1d0c268ce8b73a0babaad3894a26e50bb9dfb
Alexis Lau <40294148+alexisyslau@users.noreply.github.com>
2026-09-21 23:51:28 +0200

    cosmic ray filter for generate_off_axis_psf

--- corgihowfsc/utils/corgisim_manager.py
+++ corgihowfsc/utils/corgisim_manager.py
@@ -445,7 +445,7 @@ class CorgisimManager:
 
         return dark
     
-    def generate_off_axis_psf(self, dm1v, dm2v, dx, dy, companion_vmag=None, lind=0, exptime=1.0, gain=1):
+    def generate_off_axis_psf(self, dm1v, dm2v, dx, dy, companion_vmag=None, lind=0, exptime=1.0, gain=1, nframes=1):
         if companion_vmag is None:
             companion_vmag = self.Vmag
         
@@ -470,9 +470,32 @@ class CorgisimManager:
         else:
             # generate detector image
             detector = self.create_emccd_detector(gain)
-            master_dark = self.generate_master_dark(detector, exptime)
-            sim_scene = detector.generate_detector_image(sim_scene, exptime)
-            # sim_scene.image_on_detector.data is not gain corrected or bias subtracted
-            B = self.bias * np.ones((self.output_dim, self.output_dim))
-            return (self.k_gain*sim_scene.image_on_detector.data - B)/detector.emccd.em_gain - master_dark
 
+            # initialize cosmic ray filtering parameters
+            self._initialize_cosmic_ray_filtering()
+
+            # sim_scene.image_on_detector.data is not gain corrected or bias subtracted
+            master_dark = self.generate_master_dark(detector, exptime)
+
+            # Get the raw frames from the detector
+            raw_frames_dn = []
+            for n in range(nframes):
+                sim_scene = detector.generate_detector_image(sim_scene, exptime)
+                raw_frames_dn.append(sim_scene.image_on_detector.data)
+            
+            # Apply cosmic ray filtering
+            ProcessedFrame = onboard_processing.process_onboard_frames(
+                raw_frames_dn,
+                bias_e = self.bias,
+                e_per_dn = detector.emccd.eperdn,
+                em_gain = gain,
+                full_well_image_e = detector.emccd.full_well_image,
+                full_well_serial_e = detector.emccd.full_well_serial,
+                master_dark_e = master_dark,
+                cosmic_filter_width = self.cosmic_filter_width,
+                saturation_threshold = self.cosmic_saturation_threshold,
+                plateau_threshold = self.cosmic_plateau_threshold,
+                combine = self.frame_combine)
+
+            filtered_frame = ProcessedFrame.calibrated
+            return filtered_frame
